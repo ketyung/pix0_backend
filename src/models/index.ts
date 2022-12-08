ccd94ec28ae3249e8f99042889b765173adaa502
techchee <ketyung@techchee.com>
2022-12-08 11:37:04 +0800

    update

--- src/models/index.ts
+++ src/models/index.ts
@@ -174,10 +174,10 @@ export interface Offer {
 
     offer_id : string, 
 
-    nft_token : AccountNFToken,
+    nft_token? : AccountNFToken,
 
     price? : number, 
-    
+
     remark? : string, 
 
     start_date? : number, // in timestamp,
